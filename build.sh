d475b5d127c6e1c8e1ce47c378c4064e529e86ea
bhoomikash <b9019699@gmail.com>
2026-08-16 15:05:35 +0530

    Fix Render Flutter SDK version for web build

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-curl -fsSL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.4-stable.tar.xz -o flutter.tar.xz
+curl -fsSL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.47.0-stable.tar.xz -o flutter.tar.xz
 tar -xf flutter.tar.xz -C /tmp
 export PATH="/tmp/flutter/bin:$PATH"
 
+flutter --version
 flutter channel stable
+flutter upgrade
 flutter config --enable-web
 flutter pub get
 flutter build web --release \
